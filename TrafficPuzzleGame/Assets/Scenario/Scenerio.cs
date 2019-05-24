acdeea109ba6d8ab5fa9f28efd3f0b641afaffd3
kiria <k15081rs@apps.kct.ac.jp>
2019-05-24 16:11:11 +0900

    test

--- TrafficPuzzleGame/Assets/Scenario/Scenerio.cs
+++ TrafficPuzzleGame/Assets/Scenario/Scenerio.cs
@@ -7,7 +7,7 @@ public class Scenerio : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //test
     }
 
     // Update is called once per frame
