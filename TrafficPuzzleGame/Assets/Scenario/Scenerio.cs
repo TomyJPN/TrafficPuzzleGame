1841c0ae18f01d2364afdf1932c5a5e3b1be6aa6
kiria <k15081rs@apps.kct.ac.jp>
2019-05-31 16:17:31 +0900

    シナリオ読み込み？ 画面更新

--- TrafficPuzzleGame/Assets/Scenario/Scenerio.cs
+++ TrafficPuzzleGame/Assets/Scenario/Scenerio.cs
@@ -1,18 +1,49 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Scenerio : MonoBehaviour
 {
+    public GameObject  score_object = null;
+    public string[] vs; //シナリオ格納
+    public Text uiText;
+    public GameObject wakaba;
+
+    int CL = 0; //行番号
+
     // Start is called before the first frame update
     void Start()
     {
-        //test
+
+        Update();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (wakaba.activeSelf == false && Input.GetMouseButtonDown(0))
+        {
+            wakaba.SetActive(true);
+        }
+        if (CL < vs.Length && Input.GetMouseButtonDown(0))
+        {
+            Textupdate();
+        }
+    }
+
+    void Textupdate()
+    {
+        uiText.text = vs[CL];
+        CL++;
+        if(CL >= vs.Length)
+        {
+            End();
+        }
+    }
+
+    void End()
+    {
+        uiText.text = "The End...";
     }
 }
