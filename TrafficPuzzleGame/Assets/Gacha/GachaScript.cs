f2e4c2b8064fdf2cec0d12361d25538f9cb8568a
kiria <k15081rs@apps.kct.ac.jp>
2019-06-25 12:06:32 +0900

    UserData,GachaList,fileの仮作成 ガチャ画面のボタンとプログラムとの紐づけ

---
+++ TrafficPuzzleGame/Assets/Gacha/GachaScript.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GachaScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void MyCoin()//所持コイン
+    {
+        Debug.Log("Click");
+    }
+
+    public void GachaList()//内容一覧
+    {
+        Debug.Log("Click");
+    }
+
+    public void Probability()//確立
+    {
+        Debug.Log("Click");
+    }
+
+    public void Once()//単発
+    {
+        Debug.Log("Click");
+    }
+
+    public void TenTimes()//10連
+    {
+        Debug.Log("Click");
+    }
+
+    public void Pause()//ポーズ画面
+    {
+        Debug.Log("Click");
+    }
+}
