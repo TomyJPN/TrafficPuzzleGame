a2fe52ba28441c98be5eb3c9f12a5beff8339e47
TomyJPN <masaya120331@gmail.com>
2019-07-05 18:05:00 +0900

    タイトル画面のアニメーション

---
+++ TrafficPuzzleGame/Assets/Scenario/background.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class background : MonoBehaviour {
+  void Update() {
+    transform.Translate(2f, 0, 0);
+    if (transform.localPosition.x > 970f) {
+      transform.localPosition = new Vector3(-975f, 49.68f, 0);
+    }
+  }
+}
---
+++ TrafficPuzzleGame/Assets/Scenario/background1.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class background1 : MonoBehaviour {
+  void Update() {
+    transform.Translate(2f, 0, 0);
+    if (transform.localPosition.x > 682f) {
+      transform.localPosition = new Vector3(-1679f,0, 0);
+    }
+  }
+}
