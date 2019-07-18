5717b62189b4de5480b2293e7403e560e7becc68
TomyJPN <masaya120331@gmail.com>
2019-07-18 23:46:24 +0900

    パズルのスポーンと角度の設定

---
+++ TrafficPuzzleGame/Assets/Puzzle/Scripts/Car.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class Car : MonoBehaviour {
+  ManageMode ManageMode;
+  /// <summary>
+  /// 1=player ,2=enemy
+  /// </summary>
+  public int type;
+
+  int Mode = 0;
+  float CarSpeed = 5;
+  float CarDir;
+  public float CarX, CarY;
+
+  void Start() {
+    ManageMode = GameObject.Find("manager").GetComponent<ManageMode>();
+  }
+
+  void Update() {
+    if (type == 1) Mode = ManageMode.Mode;
+    else Mode = 1;
+    switch (Mode) {
+      case (0): break;
+      case (1): Run(); break;
+    }
+  }
+
+
+  void Run() {
+    //		Quaternion CarXY = transform.rotation;
+
+    //		print(CarXY);
+    //		float CarX
+    Vector2 CarDir = new Vector2(CarX, CarY).normalized;            //車の進行方向
+    //Vector2 CarDir = new Vector2(0, 1).normalized;      //テスト用
+    GetComponent<Rigidbody2D>().velocity = CarDir * CarSpeed; //方向*速度を代入
+  }
+}
---
+++ TrafficPuzzleGame/Assets/Puzzle/Scripts/Spawner.cs
@@ -0,0 +1,26 @@
+﻿//https://gist.github.com/hiroyukihonda/8571691
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//指定したゲームオブジェクトを無限スポーンさせるスクリプト
+public class Spawner : MonoBehaviour {
+  public GameObject Spowned;    //スポーンオブジェクトの指定
+  public float interval = 3f;       //間隔
+  public float angle;
+
+  void Start() {
+    StartCoroutine("Spawn");
+  }
+
+  IEnumerator Spawn() {
+    while (true) {
+      Quaternion rote = new Quaternion(0f, 0.0f, 0f, 1.0f);
+      GameObject obj=Instantiate(Spowned, transform.position , rote);
+      obj.transform.rotation = Quaternion.Euler(0.0f, 0.0f,angle);
+      yield return new WaitForSeconds(interval);
+    }
+  }
+
+}
